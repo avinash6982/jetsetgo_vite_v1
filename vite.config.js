8492e1265f5c3a6ad64294a56939c9b8cc5716cd
Avinash Ghosh <avinashghoshpullur@gmail.com>
2024-02-23 01:00:02 +0530

    setup

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/jetsetgo_vite_v1/",
   plugins: [react()],
   build: {
     outDir: "build",
